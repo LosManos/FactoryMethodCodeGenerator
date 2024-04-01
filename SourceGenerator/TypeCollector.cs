f8b5a8c3a3695e318f307af90a86c839796d9690
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-04-01 22:16:33 +0200

    far ut namespace

--- SourceGenerator/TypeCollector.cs
+++ SourceGenerator/TypeCollector.cs
@@ -2,6 +2,8 @@ using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+// https://gist.github.com/cmendible/9b8c7d7598f1ab0bc7ab5d24b2622622
+
 namespace SourceGenerator;
 
 public class TypeCollector : ISyntaxReceiver
@@ -17,6 +19,13 @@ public class TypeCollector : ISyntaxReceiver
         _types.ToImmutableList();
 
     private List<(IsOfType isOfType, TypeDeclarationSyntax @type, IEnumerable<AttributeSyntax> attribs)> _types { get; } = new();
+
+    public IImmutableList<(IsOfType isOfType, TypeDeclarationSyntax @type, IEnumerable<AttributeSyntax>
+        attribs)> TypesWithAttribute(params string[] attributeNames)
+    {
+        return _types.Where(t => t.attribs.Any(a => attributeNames.Contains(a.Name.ToString())))
+            .ToImmutableList();
+    }
 
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
