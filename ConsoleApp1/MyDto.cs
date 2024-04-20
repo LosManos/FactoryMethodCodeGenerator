c6075d9c1799d9f75069548fa3f43d4f77a3834f
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-04-20 11:41:29 +0200

    The first property is writen through a constructor

--- ConsoleApp1/MyDto.cs
+++ ConsoleApp1/MyDto.cs
@@ -3,7 +3,10 @@ using MyInterface;
 namespace ConsoleApp1;
 
 [Dto]
-public partial record MyRecordDto(string name);
+public partial record MyRecordDto
+{
+    public string MyValue { get; set; }
+}
 
 [Dto]
 public partial class MyClassDto(string name);
