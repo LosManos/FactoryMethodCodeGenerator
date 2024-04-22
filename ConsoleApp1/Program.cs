2df7f59b9cbf0f60d3d752f037cafe3a54527384
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-04-22 10:59:21 +0200

    Can now create class and record with constructor to use

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,5 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        var myRecord = new MyRecordDto(42, "fortytwo", 4.2f);
+        var myClass = new MyClassDto(42, "fortytwo", 4.2f);
     }
 }
