ada47bc1006dd28c7f9f9c44fbf12048c85ac059
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-04-22 16:10:10 +0200

    Cleanup the illustrative dtos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        var myRecord1 = MyRecordDto1.Create(42, "fortytwo", 4.2f);
-        var myRecord2 = MyRecordDto2.Create(42, "fortytwo", 4.2f);
-        var myRecord3 = MyRecordDto3.Create(42, "fortytwo", 4.2f);
-        var myClass = MyClassDto.Create(42, "fortytwo", 4.2f);
+        MyRecordDto_With_DifferentTypes.Create(42, "fortytwo", 4.2f);
+        MyRecordDto_With_DefaultPrivateConstructor.Create(42);
+        MyRecordDto_With_PublicPrivateConstructor.Create(42);
+        new MyRecordDto_With_PublicPrivateConstructor(42);
+        MyRecordDto_With_ExplicitPrivateConstructor.Create(42);
+
+        MyClassDto.Create(42, "fortytwo", 4.2f);
+
         // var myRecord2 = MyRecordDto2.Create(42);
     }
 }
