0dc85fb00d081736f7623e57dde0ac6b5ae80960
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-04-22 16:10:10 +0200

    Cleanup the illustrative dtos

--- ConsoleApp1/MyDto.cs
+++ ConsoleApp1/MyDto.cs
@@ -3,30 +3,27 @@ using MyInterface;
 namespace ConsoleApp1;
 
 [Dto]
-public partial record MyRecordDto1
+public partial record MyRecordDto_With_DifferentTypes
 {
     public int MyFirstValue { get; init; }
     public string MySecondValue { get; init; }
     public float MyThirdValue { get; init; }
+}
+
+[Dto]
+public partial record MyRecordDto_With_DefaultPrivateConstructor
+{
+    public int MyFirstValue { get; init; }
 }
 
 [Dto(UsePrivateConstructor = false)]
-public partial record MyRecordDto2
+public partial record MyRecordDto_With_PublicPrivateConstructor
 {
     public int MyFirstValue { get; init; }
-    public string MySecondValue { get; init; }
-    public float MyThirdValue { get; init; }
 }
 
 [Dto(UsePrivateConstructor = true)]
-public partial record MyRecordDto3
-{
-    public int MyFirstValue { get; init; }
-    public string MySecondValue { get; init; }
-    public float MyThirdValue { get; init; }
-}
-
-public partial record MyRecordThatisNotIndlucedDto
+public partial record MyRecordDto_With_ExplicitPrivateConstructor
 {
     public int MyFirstValue { get; init; }
 }
