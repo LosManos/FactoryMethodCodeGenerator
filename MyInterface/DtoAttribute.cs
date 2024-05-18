a8613748f5d3c5696e8cde6246429941793cb319
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-05-18 09:06:48 +0200

    Remove build warning

--- MyInterface/DtoAttribute.cs
+++ MyInterface/DtoAttribute.cs
@@ -5,5 +5,7 @@ public class DtoAttribute : Attribute
 {
     public bool UsePrivateConstructor { get; set; } = true;
 
+#pragma warning disable CS0414 // Field is assigned but its value is never used
     internal static readonly string UsePrivateConstructorFieldName = nameof(UsePrivateConstructor);
+#pragma warning restore CS0414 // Field is assigned but its value is never used
 }
