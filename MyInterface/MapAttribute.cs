045ef145661aa35944485c38478335789ef4b1ae
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2024-06-23 22:16:53 +0200

    WIP:Commenting

--- MyInterface/MapAttribute.cs
+++ MyInterface/MapAttribute.cs
@@ -1,6 +1,8 @@
 namespace MyInterface;
 
 /// <summary>Set this attribute on a class. Then all methods named `Map` will be mapping methods.
+/// By some, yet, unknown reason we need both the generic `TSource` and `TTarget`
+/// and the arguments `source` and `target`. It would be nice if we could get rid of one of the pairs.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class MapAttribute<TSource, TTarget>(
